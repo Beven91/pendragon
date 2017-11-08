bfcc17c07b7886dde94aadd301e54c96b38ee766
beven.zhou <beven.zhou@lianshang.com>
2017-11-08 23:27:36 +0800

    修复客户端导航到指定路由后，再刷新浏览器没有定位到对应页面视图问题

--- app/containers/router.js
+++ app/containers/router.js
@@ -1,20 +1,26 @@
 import { StackNavigator } from 'hanzojs/router'
- 
+
 module.exports = (modules) => {
   return StackNavigator({
-    Index: {
-      screen: modules.Index,
+
+    Root: {
       path: '',
-      navigationOptions: {
-        title: 'Pendragon'
-      }
-    },
-    Login: {
-      screen: modules.UserLogin,
-      path: 'login',
-      navigationOptions: {
-        title: '登录'
-      }
+      screen: StackNavigator({
+        Index: {
+          screen: modules.Index,
+          path: '',
+          navigationOptions: {
+            title: 'Pendragon'
+          }
+        },
+        Login: {
+          screen: modules.UserLogin,
+          path: 'login',
+          navigationOptions: {
+            title: '登录'
+          }
+        }
+      })
     }
   })
 }
