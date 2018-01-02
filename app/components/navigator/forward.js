31a92cbe34843b212e99d09649b23d450a85ed91
zhoubiwen <zhoubiwen@jk.pingan-inc.cn>
2018-01-02 18:22:34 +0800

    完善forward逻辑

--- app/components/navigator/forward.js
+++ app/components/navigator/forward.js
@@ -1,28 +1,41 @@
 /**
- *  用于记录路由数据，用于判断当前路由(pushstate)是前进还是后台
- *  
+ * 名称：pushState辅助工具
+ * 描述：记录路由跳转数据，用于判断当前路由(pushstate)是前进还是后台
  */
+const name = '@_StateId__@'
 
 export default class NavigateHelper {
 
+    /**
+     * 获取当前的pushStatea对应的state.id
+     */
     static getCurrentStateID() {
-        return parseInt(window.localStorage.getItem("currentStateID") || 0);
+        return parseInt(window.localStorage.getItem(name) || 0);
     }
 
+    /**
+     * 设置当前pushState的state.id
+     * @param {*} id 
+     */
     static setCurrentStateID(id) {
-        window.localStorage.setItem("currentStateID", id);
+        window.localStorage.setItem(name, id);
     }
 
+    /**
+     * 生成一个新的state.id
+     */
     static genStateID() {
-        let stateid = window.localStorage.getItem("stateid") || "0";
+        let stateid = window.localStorage.getItem(name) || "0";
         var id = parseInt(stateid) + 1;
-        window.localStorage.setItem("stateid", id);
         return id;
     }
 
+    /**
+     * 初始化state.id
+     */
     static initRoute() {
         const history = window.history;
-        const state = history.state || {};
+        const state = history.state;
         const id = (state && state.id > 0) ? state.id : this.genStateID();
         this.setCurrentStateID(id);
         if (!state) {
@@ -30,10 +43,15 @@ export default class NavigateHelper {
         }
     }
 
-    static isForward(ispopstate) {
+    /**
+     * 判断当前popstate是前进还是后退
+     * 根据当前history.state.id > 上次的state.id 如果为true则会前进 否则后退
+     * @param {Boolean} isBack 辅助判断在 history.state.id < 上次state.id时，是否有指示说明本次路由操作是后退操作
+     */
+    static isForward(isBack) {
         const history = window.history;
         const state = history.state || {};
         const isThan  = (state && state.id > this.getCurrentStateID());
-        return isThan || (!ispopstate);
+        return isThan;
     }
 }
